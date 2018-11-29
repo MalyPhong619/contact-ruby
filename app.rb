e7ecc6aaf6387e5b582791493a9fac559d55f680
Christopher Cahill and Maly Phongsavanh <christopher@>
2018-11-29 11:20:48 -0800

    go to page of contact info

--- app.rb
+++ app.rb
@@ -6,5 +6,28 @@ require('./lib/contacts')
 
 get ('/') do
   @hello = "check to see if working"
+  @list = Contact.all()
   erb(:input)
+end
+
+post ('/') do
+  @list = Contact.all()
+  first_name = params["first_name"]
+  last_name = params["last_name"]
+  job_title = params["job_title"]
+  company = params["company"]
+  contact_type = params["contact_type"]
+
+
+  contact = Contact.new(:first_name=>first_name, :last_name=>last_name, :job_title=>job_title, :company=>company, :contact_type=>contact_type)
+  contact.save()
+  @list = Contact.all()
+
+  erb(:input)
+end
+
+get ('/contacts/:id') do
+  @contact = Contact.find(params[:id])
+  @list = Contact.all()
+  erb(:contact)
 end
--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,6 +1,8 @@
 class Contact
+  @@list = []
+  @@counter = 1
 
-  attr_accessor :first_name, :last_name, :job_title, :company, :contact_type
+  attr_accessor :first_name, :last_name, :job_title, :company, :contact_type, :id
 
   def initialize(attributes)
     @first_name = attributes.fetch(:first_name)
@@ -8,6 +10,27 @@ class Contact
     @job_title = attributes.fetch(:job_title)
     @company = attributes.fetch(:company)
     @contact_type = attributes.fetch(:contact_type)
+    # @id = attributes.fetch(:id)
+    @id = @@counter
+    @@counter = @@counter + 1
   end
+
+  def self.all()
+   @@list
+  end
+
+  def save()
+    @@list.push(self)
+  end
+
+  def self.find(id)
+   contact_id = id.to_i()
+   @@list.each do |contact|
+     if contact.id == contact_id
+       return contact
+     end
+   end
+ end
+
 
 end
