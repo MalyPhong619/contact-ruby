09f883da71d426746e4238784f0b684f875c6c98
Christopher Cahill and Maly Phongsavanh <christopher@>
2018-11-29 11:59:07 -0800

    edit names

--- app.rb
+++ app.rb
@@ -17,8 +17,6 @@ post ('/') do
   job_title = params["job_title"]
   company = params["company"]
   contact_type = params["contact_type"]
-
-
   contact = Contact.new(:first_name=>first_name, :last_name=>last_name, :job_title=>job_title, :company=>company, :contact_type=>contact_type)
   contact.save()
   @list = Contact.all()
@@ -29,6 +27,15 @@ end
 get ('/contacts/:id') do
   @contact = Contact.find(params[:id])
   @list = Contact.all()
+  erb(:contact)
+end
+
+post ('/contacts/:id') do
+  first_name = params["first_name"]
+  last_name = params["last_name"]
+  @contact = Contact.find(params[:id])
+  @contact.first_name = first_name
+  @contact.last_name = last_name
   erb(:contact)
 end
 
